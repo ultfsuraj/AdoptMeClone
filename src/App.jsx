818776831448230d4938a59ad031b338e4c8afe1
R-B-Suraj <rb.suraj1kanha@gmail.com>
2024-07-02 12:29:12 +0530

    added animal filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import React from "react";
-import ReactDOM from 'react-dom'
-import Pet from './Pet'
-
+import { createRoot } from "react-dom/client";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1> Adopt Me!</h1>
-      <Pet name='Luna' animal='dog' breed='Havanese' />
-      <Pet name='Pepper' animal='bird' breed='cocktiel' />
-      <Pet name='Doink' animal='cat' breed='Mixed' />
+      <SearchParams />
     </div>
-  )
+  );
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container); // react-dom handles rendering
-root.render(React.createElement(<App/>));
+const root = createRoot(container); // react-dom handles rendering
+root.render(<App />);
