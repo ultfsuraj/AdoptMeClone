568f874409d7aea6382b9e48771baa0851a04f5e
R-B-Suraj <rb.suraj1kanha@gmail.com>
2024-07-03 10:15:44 +0530

    basic React Router

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,20 +1,25 @@
-const Pet = ({name, animal, breed, images, location, id}) => {
+import { Link } from "react-router-dom";
 
-  let hero = "http://pets-images.dev-apis.com/pets/none.jpg"
-  if(images.length){
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
     hero = images[0];
   }
 
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
+      {/* <a href={`/details/${id}`} className="pet">  this will refresh the page, slow, react has to rebuild */}
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
       <div className="info">
         <h1>{name}</h1>
-        <h2>{animal} - {breed} - {location} </h2>
+        <h2>
+          {animal} - {breed} - {location}{" "}
+        </h2>
       </div>
-    </a>
+      {/* </a> */}
+    </Link>
   );
 };
 
