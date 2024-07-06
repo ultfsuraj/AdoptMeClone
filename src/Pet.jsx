8b52da5a7d1713d345ef047f0dc416c741f5b8c2
R-B-Suraj <rb.suraj1kanha@gmail.com>
2024-07-06 14:20:38 +0530

    styled pet

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -7,12 +7,12 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
   }
 
   return (
-    <Link to={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="relative block">
       {/* <a href={`/details/${id}`} className="pet">  this will refresh the page, slow, react has to rebuild */}
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
-      <div className="info">
+      <div className="absolute bottom-0 left-0 bg-gradient-to-tr from-white to-transparent pr-2 pt-2">
         <h1>{name}</h1>
         <h2>
           {animal} - {breed} - {location}{" "}
